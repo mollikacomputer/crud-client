dabbf6f8aedc018f7a98a05ab76bd255874aae5b
mollikacomputer <mollikacomputer3@gmail.com>
2022-07-07 07:19:08 +0600

    add new user fetch post method and server connect with mongodb

--- src/Components/UpdateUser.js
+++ src/Components/UpdateUser.js
@@ -1,16 +1,45 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const UpdateUser = () => {
-    return (
-        <div>
-            <h2> Update User</h2>
-            <ul>
-                <li> <Link to='/adduser' > Add User</Link> </li>
-                <li> <Link to='/' > Back to home</Link> </li>
-            </ul>
-        </div>
-    );
+    const handleAddUser = event =>{
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const user = {name, email};
+        console.log(user);
+        fetch('http://localhost:5000/user',{
+            method: 'POST',
+            headers:{
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(user),
+        })
+        .then(res => res.json())
+        .then( data => {
+            console.log('success', data);
+            event.target.reset();
+        })
+    }
+  return (
+    <div>
+      <h2> Update User</h2>
+      <ul>
+        <li>
+          <Link to="/adduser"> Add User</Link>
+        </li>
+        <li>
+          <Link to="/"> Back to home</Link>
+        </li>
+      </ul>
+   
+        <form onSubmit={handleAddUser} style={{marginTop: "50px"}} >
+          <input type="text" name="name" placeholder="name" />
+          <input type="email" name="email" placeholder="email" />
+          <input type="submit" name="submit" value="Add User" />
+        </form>
+    </div>
+  );
 };
 
 export default UpdateUser;
